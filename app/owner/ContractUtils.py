30a30770f43d9bf885e710b27169d74226b9163f
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-06-15 13:30:03 -0300

    Update ContractUtils.py

--- app/owner/ContractUtils.py
+++ app/owner/ContractUtils.py
@@ -21,76 +21,72 @@ def getContractData(contract_name: str):
 
 # Implementando um Contrato e Retornando o Seu Endereço:
 def deployContract(w3: Web3, deployer_account, contract_name: str, *args):
-    # Verificando o Endereço da Conta do Deployer:
-    if w3.is_checksum_address(deployer_account):
-        # Recuperando os Arquivos Compilados:
-        abi, bytecode = getContractData(contract_name)
+    # Recuperando os Arquivos Compilados:
+    abi, bytecode = getContractData(contract_name)
 
-        # Implantando o Contrato:
-        Contract = w3.eth.contract(abi=abi, bytecode=bytecode)
+    # Implantando o Contrato:
+    Contract = w3.eth.contract(abi=abi, bytecode=bytecode)
 
-        # Construindo a Transação de Deploy:
-        transaction = Contract.constructor(*args).build_transaction(
-            {
-                "from": deployer_account,
-                "nonce": w3.eth.get_transaction_count(deployer_account),
-                "gasPrice": w3.eth.gas_price,
-            }
-        )
-        
-        # Assinando a Transação:
-        signed_transaction = w3.eth.account.sign_transaction(transaction, private_key=DEPLOYER_PRIVATE_KEY)
+    # Construindo a Transação de Deploy:
+    transaction = Contract.constructor(*args).build_transaction(
+        {
+            "from": deployer_account,
+            "nonce": w3.eth.get_transaction_count(deployer_account),
+            "gasPrice": w3.eth.gas_price,
+            "gas": 3000000,
+        }
+    )
+    
+    # Assinando a Transação:
+    signed_tx = w3.eth.account.sign_transaction(transaction, private_key=DEPLOYER_PRIVATE_KEY)
 
-        # Enviando a Transação:
-        tx_hash = w3.eth.send_raw_transaction(signed_transaction.raw_transaction)
-        print(f"Hash da Transação de Deploy de '{contract_name}': {tx_hash.hex()}\n")
+    # Enviando a Transação:
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+    print(f"Hash da Transação de Deploy de '{contract_name}': {tx_hash.hex()}\n")
 
-        # Esperando Pela Mineração do Bloco e Obtendo o Recibo da Transação:
-        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    # Esperando Pela Mineração do Bloco e Obtendo o Recibo da Transação:
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 
-        # Salvando o Endereço do Contrato:
-        contract_address = tx_receipt.contract_address
-        print(f"Contrato '{contract_name}' Deployado em: {contract_address}\n")
+    # Salvando o Endereço do Contrato:
+    contract_address = tx_receipt.contract_address
+    print(f"Contrato '{contract_name}' Deployado em: {contract_address}\n")
 
-        # Retonando o Contrato e Endereço:
-        return w3.eth.contract(address=contract_address, abi=abi)
-    return None
+    # Retonando o Contrato e Endereço:
+    return w3.eth.contract(address=contract_address, abi=abi)
 
 # Autorizando o Servidor de Uma Empresa na Blockchain:
 def authorizeServer(w3: Web3, contract, deployer_account, server_account):
-    if w3.is_checksum_address(deployer_account) and w3.is_checksum_address(server_account):
-        try:
-            print(f"Autorizando o Servidor: {server_account}\n")
-            tx_hash = contract.functions.authorizeRechargingServer(server_account).transact({
-                'from': deployer_account,
-                "nonce": w3.eth.get_transaction_count(deployer_account),
-                'gasPrice': w3.eth.gas_price
-            })
-            w3.eth.wait_for_transaction_receipt(tx_hash)
-            is_auth = contract.functions.isAuthorizedRechargingServer(server_account).call()
-            print(f"Estado da Autorização do Servidor '{server_account}': {is_auth}\n")
-        except Exception as e:
-            print(f"Erro ao Autorizar o Servidor '{server_account}': '{e}'\n")
+    try:
+        print(f"Autorizando o Servidor: {server_account}\n")
+        tx_hash = contract.functions.authorizeRechargingServer(server_account).transact({
+            'from': deployer_account,
+            "nonce": w3.eth.get_transaction_count(deployer_account),
+            'gasPrice': w3.eth.gas_price
+        })
+        w3.eth.wait_for_transaction_receipt(tx_hash)
+        is_auth = contract.functions.isAuthorizedRechargingServer(server_account).call()
+        print(f"Estado da Autorização do Servidor '{server_account}': {is_auth}\n")
+    except Exception as e:
+        print(f"Erro ao Autorizar o Servidor '{server_account}': '{e}'\n")
 
 # Finalizando Uma Sessão de Carregamento e Liberando os Fundos de Pagamento:
 def finishChargingSession(w3: Web3, contract, deployer_account, reservationID: int):
-    if w3.is_checksum_address(deployer_account):
-        try:
-            print(f"Finalizando a Sessão de Carregamento da Reserva: {reservationID}\n")
-            tx_hash = contract.functions.finishChargingSession(reservationID).transact({
-                'from': deployer_account,
-                "nonce": w3.eth.get_transaction_count(deployer_account),
-                'gasPrice': w3.eth.gas_price
-            })
-            w3.eth.wait_for_transaction_receipt(tx_hash)
-            cs = contract.functions.getChargingSession(reservationID).call()
-            cs_status = cs[2] # 0 = IDLE, 1 = IN_PROGRESS, 2 = FINISHED.
-            if cs_status == 2:
-                print(f"Sessão de Carregamento da Reserva '{reservationID}' Finalizada Com Sucesso!\n")
-                return True
-            else:
-                print(f"Falha ao Finalizar a Sessão de Carregamento da Reserva '{reservationID}'!\n")
-                return None
-        except Exception as e:
-            print(f"Erro ao Finalizar a Sessão de Carregamento da Reserva '{reservationID}': {e}\n")
+    try:
+        print(f"Finalizando a Sessão de Carregamento da Reserva: {reservationID}\n")
+        tx_hash = contract.functions.finishChargingSession(reservationID).transact({
+            'from': deployer_account,
+            "nonce": w3.eth.get_transaction_count(deployer_account),
+            'gasPrice': w3.eth.gas_price
+        })
+        w3.eth.wait_for_transaction_receipt(tx_hash)
+        cs = contract.functions.getChargingSession(reservationID).call()
+        cs_status = cs[2] # 0 = IDLE, 1 = IN_PROGRESS, 2 = FINISHED.
+        if cs_status == 2:
+            print(f"Sessão de Carregamento da Reserva '{reservationID}' Finalizada Com Sucesso!\n")
+            return True
+        else:
+            print(f"Falha ao Finalizar a Sessão de Carregamento da Reserva '{reservationID}'!\n")
             return None
+    except Exception as e:
+        print(f"Erro ao Finalizar a Sessão de Carregamento da Reserva '{reservationID}': {e}\n")
+        return None
