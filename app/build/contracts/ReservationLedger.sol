e66ad84572a089eca02d24ffea8c470537586b6f
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-06-16 02:00:19 -0300

    Update ReservationLedger.sol

--- app/build/contracts/ReservationLedger.sol
+++ app/build/contracts/ReservationLedger.sol
@@ -30,24 +30,12 @@ contract ReservationLedger {
         Status status;
     }
 
-    // Dados da Transação:
-    struct TransactionRecord {
-        uint256 transactionID;
-        uint256 reservationID;
-        uint256 price; // Preço da Transação em "wei".
-        address payer; // Endereço da Carteira que Efetuou o Pagamento.
-        bool paid; // Indica Se o Pagamento foi Confirmado.
-        uint256 transactionTimestamp;
-    }
-
     // Mapeamentos:
     mapping(uint256 => Reservation) public reservations;
-    mapping(uint256 => TransactionRecord) public transactionRecords;
     mapping(address => uint256[]) private customerReservationIDs;  
 
     // Contadores de IDs:
     uint256 public nextReservationID;
-    uint256 public nextTransactionID;
 
     // Eventos para Notificações:
     event ReservationCreated(
@@ -57,15 +45,8 @@ contract ReservationLedger {
         uint256 price
     );
     event ReservationStatusUpdated(uint256 indexed reservationID, Status status);
-    event TransactionRecorded(
-        uint256 indexed transactionID,
-        uint256 reservationID,
-        address payer,
-        uint256 price
-    );
 
     // Modificador:
-    // Para Criar, Confirmar, Cancelar ou Concluir Reservas, ou Registrar Transações: Apenas um dos Servidores de Carregamento ou Administrador.
     modifier onlyServerOrOwner() {
         require(authorizedServers.isAuthorizedRechargingServer(msg.sender) || msg.sender == owner, "Apenas um Dos Servidores de Carregamento ou Administrador Pode Executar Esta Acao!");
         _;
@@ -76,7 +57,6 @@ contract ReservationLedger {
         owner = msg.sender; // Define o Deployer do Contrato Como o Owner.
         authorizedServers = AuthorizedServers(_authorizedServersAddress);
         nextReservationID = 1;
-        nextTransactionID = 1;
     }
 
     // Criando uma Nova Reserva:
@@ -153,64 +133,10 @@ contract ReservationLedger {
         emit ReservationStatusUpdated(_reservationID, reservations[_reservationID].status);
     }
 
-    /**
-     * Registrando, Imutavelmente, uma Transação de Pagamento Auditável de Reserva no Ledger.
-     * Supõe-se que o Pagamento Real (Transferência de ETH/Tokens) Ocorreu.
-     * Estamos Apenas Registrando a Operação.
-     */
-    function recordTransaction(
-        uint256 _reservationID,
-        address _payer, // Endereço da Carteira Pagante.
-        bool _paid // Status de Pagamento: "true" ou "false".
-    ) public onlyServerOrOwner returns (uint256) {
-        // Verificando o Estado da Reserva:
-        require(reservations[_reservationID].reservationID != 0, "ID da Reserva Invalido!");
-        require(reservations[_reservationID].status != Status.CANCELLED, "Nao e Possivel Registrar o Pagamento de uma Reserva Cancelada!");
-        require(reservations[_reservationID].status != Status.PENDING, "Nao e Possivel Registrar o Pagamento de uma Reserva Pendente!");
-        require(reservations[_reservationID].status != Status.PAYED, "Nao e Possivel Registrar o Pagamento de uma Reserva Paga!");
-
-        // Criando a Transação:
-        uint256 newTransactionID = nextTransactionID;
-        transactionRecords[newTransactionID] = TransactionRecord({
-            transactionID: newTransactionID,
-            reservationID: _reservationID,
-            price: reservations[_reservationID].price,
-            payer: _payer,
-            paid: _paid,
-            transactionTimestamp: block.timestamp
-        });
-
-        // Atualizando o Status da Reserva:
-        reservations[_reservationID].status = Status.PAYED;
-
-        // Incrementando o ID Para a Próxima Transação:
-        nextTransactionID++;
-
-        // Emitindo a Notificação da Transação:
-        emit TransactionRecorded(
-            newTransactionID,
-            _reservationID,
-            _payer,
-            reservations[_reservationID].price
-        );
-
-        // Emitindo a Notificação da Reserva:
-        emit ReservationStatusUpdated(_reservationID, reservations[_reservationID].status);
-
-        // Retornando o ID da Transação:
-        return newTransactionID;
-    }
-
     // Função para Auditar uma Reserva:
     function getReservation(uint256 _reservationID) public view returns (Reservation memory) {
         require(reservations[_reservationID].reservationID != 0, "ID da Reserva Invalido!");
         return reservations[_reservationID];
-    }
-
-    // Função para Auditar uma Transação:
-    function getTransactionRecord(uint256 _transactionID) public view returns (TransactionRecord memory) {
-        require(transactionRecords[_transactionID].transactionID != 0, "ID de Transacao Nao Encontrado!");
-        return transactionRecords[_transactionID];
     }
 
     // Função para Retornar Auditar as Reservas de um Cliente:
