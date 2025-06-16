6732cc3570b3c9f0d8f17fcede474fbba88c7186
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-06-16 07:04:23 -0300

    Update Utils.py

--- app/ev_recharging_servers/ecocharge/Utils.py
+++ app/ev_recharging_servers/ecocharge/Utils.py
@@ -4,22 +4,20 @@
 from flask import jsonify
 from requests.exceptions import RequestException, ConnectionError, Timeout, HTTPError # Exceções Para Problemas de Conexão.
 import urllib3 # Exceções Para Problemas de Conexão.
-import asyncio
-import aiohttp
 
-# Tratando as Exceções do "HTTP" e "aiohttp":
+# Tratando as Exceções do HTTP:
 def handleHTTPExceptions(exception):
-    if isinstance (exception, urllib3.exceptions.NewConnectionError, aiohttp.ClientConnectorError):
+    if isinstance (exception, urllib3.exceptions.NewConnectionError):
         return jsonify({"error": "Não Há Caminho Até o Servidor!"}), 502 # Erro 502: Bad Gateway.
-    elif isinstance (exception, (urllib3.exceptions.MaxRetryError, ConnectionError), asyncio.TimeoutError):
+    elif isinstance (exception, (urllib3.exceptions.MaxRetryError, ConnectionError)):
         return jsonify({"error": "Servidor Alvo Está Indisponível!"}), 503 # Erro 503: Service Unavailable.
     elif isinstance (exception, Timeout):
         return jsonify({"error": "Timeout!"}), 504 # Erro 504: Gateway Timeout.
-    elif isinstance (exception, HTTPError, aiohttp.ClientResponseError):
+    elif isinstance (exception, HTTPError):
         status_code = exception.response.status_code if exception.response else 500 # Erro do HTTP ou Erro Genérico.
         reason = exception.response.reason if exception.response else "Erro Desconhecido" # Razão do Erro ou Razão Desconhecida.
         return jsonify({"error": f"Erro HTTP: '{reason}'"}), status_code
-    elif isinstance (exception, RequestException, Exception):
+    elif isinstance (exception, RequestException):
         return jsonify({"error": "Erro Genérico!"}), 500 # Erro 500: Internal Server Error.
     else:
         return jsonify({"error": "Erro Desconhecido!"}), 500
