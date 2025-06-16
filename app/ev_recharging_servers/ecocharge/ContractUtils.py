6d8e49551114526ad848fd8b0009d9b4f1280d9b
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-06-16 06:08:33 -0300

    Update ContractUtils.py

--- app/ev_recharging_servers/ecocharge/ContractUtils.py
+++ app/ev_recharging_servers/ecocharge/ContractUtils.py
@@ -47,6 +47,33 @@ def getContractData(contract_name: str):
         abi = json.loads(abi_file.read()) # Lendo Como Dicionário.
     return abi
 
+# Criando uma Reserva "Pendente" na Blockchain:
+def createReservationBlockchain(w3: Web3, contract, server_account, vehicleID: int, data: dict):
+    try:
+        print(f"Criando uma Reserva na Blockchain Para '{vehicleID}' em '{data["cityCodename"]}'\n")
+        tx_hash = contract.functions.createReservation(
+            data["chargingStationID"],
+            data["chargingPointID"],
+            data["cityCodename"],
+            data["companyName"],
+            data["chargingPointPower"],
+            data["kWhPrice"],
+            data["startTimestamp"],
+            data["finishTimestamp"],
+            data["price"],
+            data["customerAddress"]
+        ).transact({
+            'from': server_account,
+            "nonce": w3.eth.get_transaction_count(server_account),
+            'gasPrice': w3.eth.gas_price,
+            "gas": 3000000,
+            "chainId": w3.eth.chain_id
+        })
+        w3.eth.wait_for_transaction_receipt(tx_hash)
+    except Exception as e:
+        print(f"Erro ao Criar Uma Reserva na Blockchain Para '{vehicleID}' em '{data["cityCodename"]}: {e}\n")
+        return None
+
 # Iniciando Uma Sessão de Carregamento:
 def startChargingSession(w3: Web3, contract, server_account, reservationID: int):
     try:
@@ -60,7 +87,7 @@ def startChargingSession(w3: Web3, contract, server_account, reservationID: int)
         })
         w3.eth.wait_for_transaction_receipt(tx_hash)
         cs = contract.functions.getChargingSession(reservationID).call()
-        cs_status = cs[1] # 0 = IDLE, 1 = IN_PROGRESS, 2 = FINISHED.
+        cs_status = cs[2] # 0 = IDLE, 1 = IN_PROGRESS, 2 = FINISHED.
         if cs_status == 1:
             print(f"Sessão de Carregamento da Reserva '{reservationID}' Iniciada Com Sucesso!\n")
             return True
@@ -69,4 +96,27 @@ def startChargingSession(w3: Web3, contract, server_account, reservationID: int)
             return None
     except Exception as e:
         print(f"Erro ao Iniciar a Sessão de Carregamento da Reserva '{reservationID}': {e}\n")
+        return None
+
+# Marcando Uma Reserva Como Paga, Após Uma Transação:
+def markReservationAsPayed(w3: Web3, contract, server_account, reservationID: int):
+    try:
+        print(f"Marcando Uma Reserva Como Paga: {reservationID}\n")
+        tx_hash = contract.functions.markReservationAsPayed(reservationID).transact({
+            'from': server_account,
+            "nonce": w3.eth.get_transaction_count(server_account),
+            'gasPrice': w3.eth.gas_price,
+            "gas": 3000000,
+            "chainId": w3.eth.chain_id
+        })
+        w3.eth.wait_for_transaction_receipt(tx_hash)
+        rs = contract.functions.getReservation(reservationID).call()
+        rs_status = rs[11] # 0 = PENDING, 1 = CONFIRMED, 2 = PAYED, 3 = CANCELLED.
+        if rs_status == 2:
+            return True
+        else:
+            print(f"Falha ao Marcar a Reserva '{reservationID}' Como Paga!\n")
+            return None
+    except Exception as e:
+        print(f"Erro ao Marcar a Reserva '{reservationID}' Como Paga: {e}\n")
         return None
