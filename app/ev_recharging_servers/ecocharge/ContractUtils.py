ab78984805090919644ac648d517200f6a9e72e8
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-06-16 11:58:19 -0300

    Update ContractUtils.py

--- app/ev_recharging_servers/ecocharge/ContractUtils.py
+++ app/ev_recharging_servers/ecocharge/ContractUtils.py
@@ -3,7 +3,6 @@
 # Importando as Dependências:
 import os
 from web3 import Web3
-import json
 import time
 import requests
 from ReservationsManager import ReservationsManager
@@ -11,9 +10,6 @@ from ReservationsManager import ReservationsManager
 # Salvando as Informações do Owner:
 OWNER_IP = os.environ.get(f'OWNER_IP')
 OWNER_PORT = int(os.environ.get(f'OWNER_PORT'))
-
-# Caminho dos Contratos:
-CONTRACTS_DIR = 'build/contracts/'
 
 # Conectando ao Ganache e Web3:
 def connectGanacheWeb3(GANACHE_URL: str):
@@ -28,32 +24,19 @@ def connectGanacheWeb3(GANACHE_URL: str):
             print(f"Erro de Conexão ao Ganache: {e}\n")
             time.sleep(3) # Tempo de Espera Para Tentar uma Nova Conexão.
 
-# Recebendo os Endereços dos Contratos:
-def getContractsAddresses():
+# Recebendo os Contratos Compilados:
+def getContracts():
     try:
         response = requests.get(f'http://{OWNER_IP}:{OWNER_PORT}/contracts')
-        print(f"Endereços dos Contratos Recebidos Com Sucesso!\n")
+        print(f"Contratos Compilados Recebidos Com Sucesso!\n")
         return response.json()
     # Tratando as Exceções:
     except Exception as e:
-        print(f"Erro ao Receber os Endereços dos Contratos: {e}\n")
+        print(f"Erro ao Receber os Contratos Compilados: {e}\n")
         return None
 
-# Carregando o "ABI" e o "bytecode" de Um Contrato Compilado:
-def getContractData(contract_name: str):
-    with open(f"{CONTRACTS_DIR}{contract_name}.abi", 'r') as abi_file:
-        abi = json.loads(abi_file.read()) # Lendo Como Dicionário.
-    with open(f"{CONTRACTS_DIR}{contract_name}.bin", 'r') as bin_file:
-        bytecode = bin_file.read() # Lendo Como String Hexadecimal.
-    return abi, bytecode
-
 # Criando uma Reserva "Pendente" na Blockchain:
-def createReservationBlockchain(w3: Web3, contract, server_account, vehicleID: int, data: dict):
-    # Recuperando os Arquivos Compilados:
-    abi, bytecode = getContractData("ReservationLedger")
-
-    # Implantando o Contrato:
-    Contract = w3.eth.contract(abi=abi, bytecode=bytecode)
+def createReservationBlockchain(w3: Web3, rl_contract, server_account, vehicleID: int, data: dict):
     try:
         # Verificando os Dados:
         assert isinstance(data["startTimestamp"], int)
@@ -62,7 +45,7 @@ def createReservationBlockchain(w3: Web3, contract, server_account, vehicleID: i
         assert Web3.is_address(data["customerAddress"]), "Endereço inválido"
         # Enviando os Dados:
         print(f"Criando uma Reserva na Blockchain Para '{vehicleID}' em '{data["cityCodename"]}'\n")
-        tx_hash = Contract.functions.createReservation(
+        tx_hash = rl_contract.functions.createReservation(
             data["chargingStationID"],
             data["chargingPointID"],
             data["cityCodename"],
@@ -86,10 +69,10 @@ def createReservationBlockchain(w3: Web3, contract, server_account, vehicleID: i
         return None
 
 # Iniciando Uma Sessão de Carregamento:
-def startChargingSession(w3: Web3, contract, server_account, reservationID: int):
+def startChargingSession(w3: Web3, csm_contract, server_account, reservationID: int):
     try:
         print(f"Iniciando a Sessão de Carregamento da Reserva: {reservationID}\n")
-        tx_hash = contract.functions.startChargingSession(reservationID).transact({
+        tx_hash = csm_contract.functions.startChargingSession(reservationID).transact({
             'from': server_account,
             "nonce": w3.eth.get_transaction_count(server_account),
             'gasPrice': w3.eth.gas_price,
@@ -97,7 +80,7 @@ def startChargingSession(w3: Web3, contract, server_account, reservationID: int)
             "chainId": w3.eth.chain_id
         })
         w3.eth.wait_for_transaction_receipt(tx_hash)
-        cs = contract.functions.getChargingSession(reservationID).call()
+        cs = csm_contract.functions.getChargingSession(reservationID).call()
         cs_status = cs[2] # 0 = IDLE, 1 = IN_PROGRESS, 2 = FINISHED.
         if cs_status == 1:
             print(f"Sessão de Carregamento da Reserva '{reservationID}' Iniciada Com Sucesso!\n")
@@ -108,7 +91,6 @@ def startChargingSession(w3: Web3, contract, server_account, reservationID: int)
     except Exception as e:
         print(f"Erro ao Iniciar a Sessão de Carregamento da Reserva '{reservationID}': {e}\n")
         return None
-
 
 # Marcando Todas as Reservas Pendentes de Um Usuário Como Confirmadas e Criando o Escrow:
 def markReservationsAsConfirmed(w3: Web3, rl_contract, escrow_contract, server_account, customerAddress, company_accounts):
@@ -187,10 +169,10 @@ def markReservationsAsCanceled(w3: Web3, rl_contract, server_account, customerAd
                 print(f"Erro ao Marcar a Reserva '{int(res['reservationID'])}' Como Cancelada: {e}\n")
 
 # Marcando Uma Reserva Como Paga, Após Uma Transação:
-def markReservationAsPayed(w3: Web3, contract, server_account, reservationID: int):
+def markReservationAsPayed(w3: Web3, rl_contract, server_account, reservationID: int):
     try:
         print(f"Marcando Uma Reserva Como Paga: {reservationID}\n")
-        tx_hash = contract.functions.markReservationAsPayed(reservationID).transact({
+        tx_hash = rl_contract.functions.markReservationAsPayed(reservationID).transact({
             'from': server_account,
             "nonce": w3.eth.get_transaction_count(server_account),
             'gasPrice': w3.eth.gas_price,
@@ -198,7 +180,7 @@ def markReservationAsPayed(w3: Web3, contract, server_account, reservationID: in
             "chainId": w3.eth.chain_id
         })
         w3.eth.wait_for_transaction_receipt(tx_hash)
-        rs = contract.functions.getReservation(reservationID).call()
+        rs = rl_contract.functions.getReservation(reservationID).call()
         rs_status = rs[11] # 0 = PENDING, 1 = CONFIRMED, 2 = PAYED, 3 = CANCELLED.
         if rs_status == 2:
             return True
